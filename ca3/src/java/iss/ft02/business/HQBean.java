e2b83c72085e6872b228b2228f24bfec5503b18a
TranNgocHieu <hieu2608@gmail.com>
2016-11-07 17:03:33 +0800

    Multipart

--- ca3/src/java/iss/ft02/business/HQBean.java
+++ ca3/src/java/iss/ft02/business/HQBean.java
@@ -7,6 +7,13 @@ package iss.ft02.business;
 
 import iss.ft02.entity.Pod;
 import iss.ft02.servlet.Callback;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Timestamp;
 import java.util.Optional;
 import javax.annotation.Resource;
@@ -18,6 +25,7 @@ import javax.ejb.Timeout;
 import javax.ejb.Timer;
 import javax.ejb.TimerHandle;
 import javax.ejb.TimerService;
+import javax.imageio.ImageIO;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -29,7 +37,12 @@ import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
-import sun.net.www.http.HttpClient;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
 
 /**
  *
@@ -41,28 +54,45 @@ public class HQBean {
     private Client client = ClientBuilder.newClient();
 //<<<<<<< HEAD
     @Resource TimerService timerService;
-    public void foward(Pod pod){
+    public void foward(Pod pod) throws FileNotFoundException, IOException{
         String url = "10.10.0.50:8080/epod/upload";
 //        UriBuilder builder = UriBuilder.fromResource(Callback.class);
 //        String callback = builder.scheme("http").host("{server}").port("{port}");
         String callback = "http://10.10.24.154:8080/epod/callback";
         System.out.println(">>> Start forwarding to HQ");
-
-        WebTarget target = client.target(url);
-       
-        MultivaluedMap<String, Object> form = new MultivaluedHashMap<>(); 
-        form.add("teamId", "8db3c30e");
-        form.add("podId", String.valueOf(pod.getPodId()));
-        form.add("callback", callback);
-        form.add("note",pod.getNote());
-        form.add("image", pod.getImage());
+        Client client = ClientBuilder.newBuilder()
+                                    .register(MultiPartFeature.class)
+                                    .build();
         
+        MultiPart multiPart = new MultiPart();
+        
+        FileOutputStream fos = new FileOutputStream(pod.getDeliver().getName());
+        fos.write(pod.getImage());
+        fos.close();
+        
+        FileDataBodyPart imgPart = new FileDataBodyPart("image",
+                                            new File(pod.getDeliver().getName()),
+                                            MediaType.APPLICATION_OCTET_STREAM_TYPE);
+        imgPart.setContentDisposition(FormDataContentDisposition.name("image").fileName("ca3.png").build());
+        MultiPart formPart = new FormDataMultiPart()
+                                                    .field("teamId", "8db3c30e", MediaType.TEXT_PLAIN_TYPE)
+                                                    .field("podId", String.valueOf(pod.getPodId()), MediaType.TEXT_PLAIN_TYPE)
+                                                    .field("callback", callback, MediaType.TEXT_PLAIN_TYPE)
+                                                    .field("note", pod.getNote(), MediaType.TEXT_PLAIN_TYPE)
+                                                    .bodyPart(imgPart);
+        formPart.setMediaType(MediaType.MULTIPART_FORM_DATA_TYPE);
+        multiPart.bodyPart(formPart);
+        
+        System.out.println(">>> BodyPart form: " + formPart.toString());
 //        Invocation.Builder request = target.request(MediaType.APPLICATION_FORM_URLENCODED)
 //                .post(Entity.form(form), Response.class);
-
-        Response resp = client.target(url)
-                              .request(MediaType.MULTIPART_FORM_DATA)
-                              .post(Entity.entity(form, MediaType.MULTIPART_FORM_DATA));
+        WebTarget target = client.target(url);
+        
+        Invocation.Builder inv = target.request();
+        
+        Response resp = inv.post(Entity.entity(formPart, formPart.getMediaType()));
+        
+        System.out.println(">> call Resp" + resp.getStatus());
 //=======
 //    @Resource
 //    TimerService timerService;
